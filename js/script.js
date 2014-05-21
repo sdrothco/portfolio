cd7e0fe8e401d0f1cc0cf1449d206bb5398a0397
sdrothco <sdrothco@gmail.com>
2014-05-21 12:29:21 -0600

    Initial commit.  Has portfolio images and sample data.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	$('#fullpage').fullpage({
+		resize: false,
+		autoScrolling: false,
+		//scrollOverflow: true,
+		menu: '#menu',
+		anchors: ['aboutPage', 'portfolioPage', 'skillsPage', 'contactPage']
+		//navigation: true,
+		//navigationPosition: 'right',
+		//navigationTooltips: ['About', 'Portfolio', 'Skills', 'Contact']
+	});
+
+	$('.project').hover( function() {
+		$(this).find('.project-img-overlay').show();
+		$(this).find('.project-img-text').show();
+
+	}).mouseleave( function() {
+		$(this).find('.project-img-overlay').hide();
+		$(this).find('.project-img-text').hide();
+
+	});
+
+});
